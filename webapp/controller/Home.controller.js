87f85f312c1d6bc9b270913ecb5c20fcd71b482a
bsrcs <bsr.ercelik@gmail.com>
2020-08-13 22:08:44 +0300

    add delete button

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,50 +1,60 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Fragment",
+    "sap/ui/core/Fragment"
   ],
-  function (Controller, JSONModel, Fragment) {
+  function (Controller, History, JSONModel, Fragment) {
     "use strict"
     return Controller.extend("workerapp.controller.Home", {
       onInit: function () {
         //runs one time for a view
         //model for the table
         this.tableDataModel = new JSONModel({
-          table: [
-            {
-              name: "Jane",
-              lastName: "Smith",
-              email: "a@b.com",
-              country: "Turkey",
-            },
-          ],
+          table: [],
         })
         //set the model to the root of the view
         this.getView().setModel(this.tableDataModel, "tableModelPath")
         this.formDataModel = new JSONModel({
           form: {
-            name: "Joe",
-            lastName: "Smith",
-            email: "a@b.com",
-            country: "Turkey",
+            id:"",
+            name: "",
+            lastName: "",
+            email: "",
+            country: "Turkey"
           },
-        })
+        });
         this.getView().setModel(this.formDataModel, "formModelPath")
 
         /** Edit Form Model */
         this.editDataModel = new JSONModel({
           form: {
-            name: "a",
+            id:"",
+            name: "",
             lastName: "",
             email: "",
-            country: "",
+            country: ""
           },
         })
         this.getView().setModel(this.editDataModel, "editModelPath")
       },
 
-      onBack: function (evt) {},
+      /** Back button */
+      onNavBack: function () {
+        var oHistory = History.getInstance()
+        var sPreviousHash = oHistory.getPreviousHash()
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1)
+        } else {
+          var oRouter = UIComponent.getRouterFor(this)
+          oRouter.navTo("overview", {}, true)
+        }
+      },
+
+
+
       handleCancel: function () {},
 
       handleSave: function () {
@@ -55,13 +65,20 @@ sap.ui.define(
         console.log(
           "data extracted from the form is: " + JSON.stringify(jsonDataFromView)
         )
-
+        /** create new object for each row you are going to create(updating the object) */
+        var newObject2 ={
+            id: new Date().getTime(),
+            name:  jsonDataFromView.name,
+            lastName:  jsonDataFromView.lastName,
+            email:  jsonDataFromView.email,
+            country:  jsonDataFromView.country
+        };
+        
         var tableDataModelExtracted = this.getView().getModel("tableModelPath")
         var newTableData = tableDataModelExtracted.getData()
         console.log("tableDataModelExtracted: " + JSON.stringify(newTableData))
-
         //add the data from the form to the array received from the table
-        newTableData.table.push(jsonDataFromView)
+        newTableData.table.push(newObject2);
         console.log("new table data : " + JSON.stringify(newTableData))
         //set the new data to the table's data model
         this.getView().getModel("tableModelPath").setData(newTableData)
@@ -99,23 +116,52 @@ sap.ui.define(
         }
       },
       onCloseDialog: function () {
-        var editModelData = this.getView().getModel("editModelPath").getData()
-          .form
-        var tableModel = this.getView().getModel("tableModelPath")
-        var tableList = tableModel.getData().table
+        var editModelData = this.getView().getModel("editModelPath").getData().form;
+        var tableModel = this.getView().getModel("tableModelPath");
+        var tableList = tableModel.getData().table;
         //modify data and set the new data to the table model
         tableList.forEach(function (item, index) {
-          if (item.name == editModelData.name) {
-            console.log("index "+index+" was edited!");
-            tableList[index] = editModelData
+          if (item.id == editModelData.id) {
+            var newObject={
+              id:editModelData.id,
+              name: editModelData.name,
+              lastName: editModelData.lastName,
+              email: editModelData.email,
+              country: editModelData.country
+            }
+            console.log("index " + index + " was edited!")
+            tableList[index] = newObject;
           }
         })
         // set data in right structure
-        tableModel.setData({"table":tableList});
+        tableModel.setData({ table: tableList })
         console.log(JSON.stringify(editModelData))
         console.log("editing completed!")
         this.byId("editDialog").close()
       },
+      handleDelete: function(event){
+        //get the row that was clicked
+        var row = event.getSource()
+        var context = row.getBindingContext("tableModelPath")
+        var itemToDelete = context.oModel.getProperty(context.sPath)
+        console.log(itemToDelete);
+        var newTableList =[];
+        var indexofNewTable = 0;
+
+        // delete the item that is selected
+        var tableModel = this.getView().getModel("tableModelPath");
+        var tableList = tableModel.getData().table;
+        tableList.forEach(function (item, index) {
+          if (item.id !== itemToDelete.id) {
+            newTableList[indexofNewTable++] = item;
+          }
+        });
+        console.log(newTableList);
+        
+        // set the updated list to the table model 
+        tableModel.setData({table: newTableList});
+      }
+        
     })
   }
 )
